cacf1c42814670d5fc9bd73017f1b7b4d2d765f7
Kwonjaeseong <77simon@naver.com>
2024-07-05 14:25:22 +0900

    init

--- js/main.js
+++ js/main.js
@@ -1,107 +1,27 @@
 $(document).ready(function () {
-  let winWidth = jQuery(window).width();
-
   // Rolling Banner
+
   let recommendSlider = $("#recommendedPlaylist");
   recommendSlider.marquee({
-    speed: 500,
+    speed: 1000,
     duplicated: true,
     allowCss3Support: true,
     gap: 0,
     startVisible: true,
     direction: "left",
-    pauseOnHover: false,
+    pauseOnHover: true,
   });
-
-  let playListDom = $("#recommendedPlaylist");
-
-  if (winWidth > 1023) {
-    playListDom.on({
-      mouseenter: function () {
-        recommendSlider.marquee("pause");
-      },
-      mouseleave: function () {
-        recommendSlider.marquee("resume");
-      },
-    });
-  } else {
-    playListDom.off({
-      mouseenter: function () {
-        recommendSlider.marquee("pause");
-      },
-      mouseleave: function () {
-        recommendSlider.marquee("resume");
-      },
-    });
-  }
-
-  // //
-
-  // let recommendSlider02 = $('#recommendedPlaylist02');
-  // recommendSlider02.marquee({
-  //     speed: 500,
-  //     duplicated: true,
-  //     allowCss3Support: true,
-  //     gap: 0,
-  //     startVisible: true,
-  //     direction: 'left',
-  //     pauseOnHover: false
-  // });
-
-  // let playListDom02 = $('#recommendedPlaylist02');
-
-  // if (winWidth > 1023) {
-  //     playListDom02.on({
-  //         mouseenter: function () {
-  //             recommendSlider02.marquee('pause');
-  //         },
-  //         mouseleave: function () {
-  //             recommendSlider02.marquee('resume');
-  //         }
-  //     });
-  // } else {
-  //     playListDom02.off({
-  //         mouseenter: function () {
-  //             recommendSlider02.marquee('pause');
-  //         },
-  //         mouseleave: function () {
-  //             recommendSlider02.marquee('resume');
-  //         }
-  //     });
-  // }
-
-  //
-
-  // let recommendSlider03 = $('#recommendedPlaylist03');
-  // recommendSlider03.marquee({
-  //     speed: 500,
-  //     duplicated: true,
-  //     allowCss3Support: true,
-  //     gap: 0,
-  //     startVisible: true,
-  //     direction: 'left',
-  //     pauseOnHover: false
-  // });
-
-  // let playListDom03 = $('#recommendedPlaylist03');
-
-  // if (winWidth > 1023) {
-  //     playListDom03.on({
-  //         mouseenter: function () {
-  //             recommendSlider03.marquee('pause');
-  //         },
-  //         mouseleave: function () {
-  //             recommendSlider03.marquee('resume');
-  //         }
-  //     });
-  // } else {
-  //     playListDom03.off({
-  //         mouseenter: function () {
-  //             recommendSlider03.marquee('pause');
-  //         },
-  //         mouseleave: function () {
-  //             recommendSlider03.marquee('resume');
-  //         }
-  //     });
-  // }
 });
+
+// $('.marquee').marquee({
+// 	//marquee 지속시간
+// 	duration: 15000,
+// 	//복제된 요소와의 간격
+// 	gap: 50,
+// 	//시작 delay시간
+// 	delayBeforeStart: 0,
+// 	//marquee방향
+// 	direction: 'left',
+// 	//요소 복제 여부
+// 	duplicated: true
+// });
